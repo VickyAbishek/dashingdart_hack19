31a38a3903b0d7cc96aeb5d8a8a4a2a4e3de4a1c
VickyAbishek <vicky.abishek@outlook.com>
2019-06-01 14:49:32 +0530

    Community Page

---
+++ dashingdir/lib/gallery.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'imagerotater.dart';
+
+
+class GalleryView extends StatefulWidget {
+  @override
+  _GalleryViewState createState() => _GalleryViewState();
+}
+
+class _GalleryViewState extends State<GalleryView> {
+  var currentIndex = 1;
+  @override
+  Widget build(BuildContext context) {
+
+    var imageList = [
+    "https://uae.microless.com/cdn/no_image.jpg",
+    "https://images-na.ssl-images-amazon.com/images/I/81aF3Ob-2KL._UX679_.jpg",
+    "https://www.boostmobile.com/content/dam/boostmobile/en/products/phones/apple/iphone-7/silver/device-front.png.transform/pdpCarousel/image.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgUgs8_kmuhScsx-J01d8fA1mhlCR5-1jyvMYxqCB8h3LCqcgl9Q",
+    "https://ae01.alicdn.com/kf/HTB11tA5aiAKL1JjSZFoq6ygCFXaw/Unlocked-Samsung-GALAXY-S2-I9100-Mobile-Phone-Android-Wi-Fi-GPS-8-0MP-camera-Core-4.jpg_640x640.jpg",
+    "https://media.ed.edmunds-media.com/gmc/sierra-3500hd/2018/td/2018_gmc_sierra-3500hd_f34_td_411183_1600.jpg",
+    "https://hips.hearstapps.com/amv-prod-cad-assets.s3.amazonaws.com/images/16q1/665019/2016-chevrolet-silverado-2500hd-high-country-diesel-test-review-car-and-driver-photo-665520-s-original.jpg",
+    "https://www.galeanasvandykedodge.net/assets/stock/ColorMatched_01/White/640/cc_2018DOV170002_01_640/cc_2018DOV170002_01_640_PSC.jpg",
+    "https://media.onthemarket.com/properties/6191869/797156548/composite.jpg",
+    "https://media.onthemarket.com/properties/6191840/797152761/composite.jpg",
+     ];
+    
+
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.chevron_left, size: 40,),
+            onPressed: () {
+              setState(() {
+                print(currentIndex);
+                if(currentIndex != 0 ){
+                  currentIndex = --currentIndex; }
+                print(currentIndex);
+              });
+              
+            },
+          ),
+          new Container(
+            width: MediaQuery.of(context).size.width * 60,
+            height: 200,
+            child:Image.network(imageList[currentIndex]),
+            // child: ImageRotater(imageList),
+          ),
+          new IconButton(
+            icon: new Icon(Icons.chevron_right, size: 40),
+            onPressed: () {
+              setState(() {
+                print(currentIndex);
+                if(currentIndex !=imageList.length-1 ){
+                    currentIndex = ++currentIndex;
+                }
+                print(currentIndex);
+              });
+            },
+          )
+        ],
+      ),
+    );
+
+  }
+}
--- dashingdir/lib/main.dart
+++ dashingdir/lib/main.dart
@@ -1,111 +1,252 @@
 import 'package:flutter/material.dart';
+import 'package:dashingdir/gallery.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AboutCommunity());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class AboutCommunity extends StatelessWidget {
+  String communityName = "Pune Flutter Group";
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:Scaffold(
+        appBar: new AppBar(
+          title: Text(communityName),
+        ),
+        
+        body: ListView(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            AboutUsHeader(),
+                  CommunityDetails(),
+                  LocationHeader(),
+                  LocationDetails(),
+                  SocialMediaHeader(),
+                  SocialMediaIcons(),
+                  GalleryHeader(),
+                  GalleryView()
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+        floatingActionButton: new FloatingActionButton(
+          elevation: 0.0,
+          child: new Icon(Icons.chat),
+          backgroundColor: new Color(0xFF2196F3),
+          onPressed: (){}
+        ),
+      )
     );
+  }
+}
+
+class CommunityDetails extends StatelessWidget {
+  String aboutUsContent = "Asdf asd fasd fsd gadsg ag to query , your widget will be scheduled to be rebuilt, keeping your widget up-to-dateYou can use this function to query the size an orientation of the screen. When that information changes, your widget will be scheduled to be rebuilt, keeping your widget up-to-date.You can use this function to query the size an orientation of the screen. When that information changes, your widget will be scheduled to be rebuilt, keeping your widget up-to-date.";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.all(15.0),
+        decoration: new BoxDecoration(
+          color: const Color(0xFF42a5f5),
+          // color: Colors.white,
+          boxShadow: [new BoxShadow(
+            color: Colors.black,
+            blurRadius: 20.0,
+          ),]
+        ),
+        // margin: EdgeInsets.only(left: 15.0, right: 15.0),
+        padding: EdgeInsets.all(10.0),
+        width: MediaQuery.of(context).size.width * 0.80,
+        alignment: Alignment.center,
+        child: Text(
+              aboutUsContent,
+              style: new TextStyle(fontSize:14.0,
+              // color: const Color(0xFFf5f5f5),
+              color: Colors.black,
+              // color: Colors.white,
+              fontWeight: FontWeight.w500,
+              fontFamily: "Roboto")
+        ),
+      );
+  }
+}
+
+class AboutUsHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child:Text(
+          ("About Us"),
+          style: new TextStyle(
+          fontSize:18.0,    
+          // color: const Color(0xFFf5f5f5),
+          color: Colors.black,
+          fontWeight: FontWeight.w500,
+          fontFamily: "Roboto"),
+          textAlign: TextAlign.center,
+        )
+    );
+  }
+}
+
+class LocationHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child:Text(
+          ("Location"),
+          style: new TextStyle(
+          fontSize:18.0,    
+          // color: const Color(0xFFf5f5f5),
+          color: Colors.black,
+          fontWeight: FontWeight.w500,
+          fontFamily: "Roboto"),
+          textAlign: TextAlign.center,
+        )
+    );
+  }
+}
+
+class SocialMediaHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child:Text(
+          ("Social Media Links"),
+          style: new TextStyle(
+          fontSize:18.0,    
+          // color: const Color(0xFFf5f5f5),
+          color: Colors.black,
+          fontWeight: FontWeight.w500,
+          fontFamily: "Roboto"),
+          textAlign: TextAlign.center,
+        )
+    );
+  }
+}
+
+class GalleryHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child:Text(
+          ("Gallery"),
+          style: new TextStyle(
+          fontSize:18.0,    
+          // color: const Color(0xFFf5f5f5),
+          color: Colors.black,
+          fontWeight: FontWeight.w500,
+          fontFamily: "Roboto"),
+          textAlign: TextAlign.center,
+        )
+    );
+  }
+}
+
+class LocationDetails extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      decoration: new BoxDecoration(
+          color: const Color(0xFF42a5f5),
+          boxShadow: [new BoxShadow(
+            color: Colors.black,
+            blurRadius: 20.0,
+          ),]
+        ),
+      padding: EdgeInsets.all(10.0),
+      margin: EdgeInsets.all(10.0),
+      width: MediaQuery.of(context).size.width * 0.80,
+      alignment: Alignment.center,  
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width * 0.60,
+              child: LocationText(),
+            ),
+            Icon(Icons.location_on, size: 40),
+        ],
+      )
+    );
+  }
+}
+
+class LocationText extends StatelessWidget {
+  String locationDetails = "Marigold complex, Wadgaon Sheri, Pune, Maharashtra 411014";
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      locationDetails,
+      style: new TextStyle(fontSize:12.0,
+      // color: const Color(0xFFf5f5f5),
+      color: Colors.black,
+      fontWeight: FontWeight.w500,
+      fontFamily: "Roboto"),
+      textAlign: TextAlign.left,
+    );
+  }              
+}
+
+class SocialMediaIcons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.80,
+      padding: EdgeInsets.all(10.0),
+      margin: EdgeInsets.all(10.0),
+      alignment: Alignment.center,
+      decoration: new BoxDecoration(
+          color: const Color(0xFF42a5f5),
+          boxShadow: [new BoxShadow(
+            color: Colors.black,
+            blurRadius: 20.0,
+          ),]
+        ),
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          // whatsapp/ fb/ telegram twitter
+          Icon(Icons.whatshot, size: 40),
+          Icon(Icons.face, size: 40),
+          Icon(Icons.wifi, size: 40),
+          Icon(Icons.phone, size: 40)
+        ],
+      ),
+    );
+  } 
+}
+
+class ImageView extends StatelessWidget {
+
+  var imageList = [
+    "https://uae.microless.com/cdn/no_image.jpg",
+    "https://images-na.ssl-images-amazon.com/images/I/81aF3Ob-2KL._UX679_.jpg",
+    "https://www.boostmobile.com/content/dam/boostmobile/en/products/phones/apple/iphone-7/silver/device-front.png.transform/pdpCarousel/image.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgUgs8_kmuhScsx-J01d8fA1mhlCR5-1jyvMYxqCB8h3LCqcgl9Q",
+    "https://ae01.alicdn.com/kf/HTB11tA5aiAKL1JjSZFoq6ygCFXaw/Unlocked-Samsung-GALAXY-S2-I9100-Mobile-Phone-Android-Wi-Fi-GPS-8-0MP-camera-Core-4.jpg_640x640.jpg",
+    "https://media.ed.edmunds-media.com/gmc/sierra-3500hd/2018/td/2018_gmc_sierra-3500hd_f34_td_411183_1600.jpg",
+    "https://hips.hearstapps.com/amv-prod-cad-assets.s3.amazonaws.com/images/16q1/665019/2016-chevrolet-silverado-2500hd-high-country-diesel-test-review-car-and-driver-photo-665520-s-original.jpg",
+    "https://www.galeanasvandykedodge.net/assets/stock/ColorMatched_01/White/640/cc_2018DOV170002_01_640/cc_2018DOV170002_01_640_PSC.jpg",
+    "https://media.onthemarket.com/properties/6191869/797156548/composite.jpg",
+    "https://media.onthemarket.com/properties/6191840/797152761/composite.jpg",
+  ];
+
+  var currentIndex = 0;
+  ImageView(this.currentIndex);
+  @override
+  Widget build(BuildContext context ) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.10,
+      height: 200,
+      child: Image.network(imageList[currentIndex]));
+  
   }
 }
